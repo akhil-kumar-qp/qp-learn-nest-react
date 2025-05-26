effdc19bf4906a404ba90c352c3340a77f882a43
Akhil Kumar <akhil.kumar@questionpro.com>
2025-05-26 17:11:36 +0530

    feat: Till step 8

---
+++ frontends/main-app/src/providers/AppProviders.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const AppProviders: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  // Create a client using ref to avoid re-creating it on every render
+  return <>{children}</>;
+};
